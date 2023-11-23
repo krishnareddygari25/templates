b6c0458be6965799bb4a386f9b469f59ab803b55
krishnareddygari25 <krishnareddygarianusha@gmail.com>
2023-11-23 15:09:33 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+def anusha(request):
+    return render(request,'anusha.html')
+def attributes(request):
+    return render(request,'attributes.html')
+def inline(request):
+    return render(request,'inline.html')
+def internal(request):
+    return render(request,'internal.html')
+def id(request):
+    return render(request,'id.html')
+def anu(request):
+    return render(request,'anu.html')
+def pseudo(request):
+    return render(request,'pseudo.html')
+def box(request):
+    return render(request,'box.html')
+def anil(request):
+    return render(request,'anil.html')
+def tables(request):
+    return render(request,'tables.html')
+def mom(request):
+    return render(request,'mom.html')
+def nanna(request):
+    return render(request,'nanna.html')
+
